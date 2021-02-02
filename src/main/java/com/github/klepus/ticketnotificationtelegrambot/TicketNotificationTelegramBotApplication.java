0a79c39e833061bd4e827896e71f15dfbf58b56b
Aleks Klepinin <aleks.klepinin@gmail.com>
2021-02-02 13:55:11 +0300

    TNTB-0: Added SpringBoot skeleton to the project

---
+++ src/main/java/com/github/klepus/ticketnotificationtelegrambot/TicketNotificationTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.klepus.ticketnotificationtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketNotificationTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketNotificationTelegramBotApplication.class, args);
+	}
+
+}
