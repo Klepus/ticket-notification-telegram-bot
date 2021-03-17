841e75f50040dd1dbe3eb030a3ec71da07e0c718
Aleks Klepinin <aleks.klepinin@gmail.com>
2021-03-17 15:46:11 +0300

    TNTB-1: added repository layer.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM adoptopenjdk/openjdk11:ubi
 ARG JAR_FILE=target/*.jar
 ENV BOT_NAME=KlepininTestBot
-ENV BOT_TOKEN=1498375289:AAFzu-l63J-_e4tRwhK2d-waLdSR7bO9BlM
+ENV BOT_TOKEN=1498375289:AAHyz4JiRebFRTB4rvmePF4p6cyL9AW1s9I
+ENV BOT_DB_USERNAME=tntb_db_user
+ENV BOT_DB_PASSWORD=tntb_db_password
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-Dspring.datasource.password=${BOT_DB_PASSWORD}", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-Dspring.datasource.username=${BOT_DB_USERNAME}", "-jar", "app.jar"]
--- src/test/java/com/github/klepus/ticketnotificationtelegrambot/command/CommandContainerTest.java
+++ src/test/java/com/github/klepus/ticketnotificationtelegrambot/command/CommandContainerTest.java
@@ -2,6 +2,7 @@ package com.github.klepus.ticketnotificationtelegrambot.command;
 
 import com.github.klepus.ticketnotificationtelegrambot.service.SendBotMessageService;
 import com.github.klepus.ticketnotificationtelegrambot.service.SendBotMessageServiceImpl;
+import com.github.klepus.ticketnotificationtelegrambot.service.TelegramUserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,7 +19,8 @@ class CommandContainerTest {
     @BeforeEach
     public void init() {
         SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageServiceImpl.class);
-        commandContainer = new CommandContainer(sendBotMessageService);
+        TelegramUserService telegramUserService = Mockito.mock(TelegramUserService.class);
+        commandContainer = new CommandContainer(sendBotMessageService, telegramUserService);
     }
 
     @Test
